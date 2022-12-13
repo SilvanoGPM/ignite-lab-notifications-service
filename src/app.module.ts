018a6e70a415fd9f4ea23758bfedf69d05cccc27
SkyG0D <silvanosilvino@hotmail.com>
2022-12-12 23:08:29 -0300

    feat: dependency inject and inversion example

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { MailService } from './mail/mail.service';
+import { PostmarkMailService } from './mail/postmark-mail.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: MailService,
+      useClass: PostmarkMailService,
+    },
+  ],
+})
+export class AppModule {}
