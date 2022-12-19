1e11ec6e68d7f15cc6ddbf583f08bcb8319dddec
SkyG0D <silvanosilvino@hotmail.com>
2022-12-19 00:24:12 -0300

    feat: routes to controller

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,11 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Patch,
+  HttpCode,
+} from '@nestjs/common';
+
+import { CancelNotificationUseCase } from '@app/use-cases/cancel-notification-use-case';
+import { GetRecipientNotificationsUseCase } from '@app/use-cases/get-recipient-notifications-use-case';
+import { CountRecipientNotificationsUseCase } from '@app/use-cases/count-recipient-notifications-use-case';
+import { ReadNotificationUseCase } from '@app/use-cases/read-notification-use-case';
+import { UnreadNotificationUseCase } from '@app/use-cases/unread-notification-use-case';
 import { SendNotificationUseCase } from '@app/use-cases/send-notification-use-case';
 import { CreateNotificationDto } from '../dtos/create-notification.dto';
 import { NotificationMapper } from '../mappers/notification-mapper';
 
 @Controller('notifications')
 export class NotificationController {
-  constructor(private sendNotification: SendNotificationUseCase) {}
+  constructor(
+    private sendNotification: SendNotificationUseCase,
+    private getRecipientNotifications: GetRecipientNotificationsUseCase,
+    private countRecipientNotifications: CountRecipientNotificationsUseCase,
+    private readNotification: ReadNotificationUseCase,
+    private unreadNotification: UnreadNotificationUseCase,
+    private cancelNotification: CancelNotificationUseCase,
+  ) {}
+
+  @Get(':recipientId/find')
+  async findManyByRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getRecipientNotifications.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notifications.map(NotificationMapper.toHttp),
+    };
+  }
+
+  @Get(':recipientId/count')
+  async countByRecipient(@Param('recipientId') recipientId: string) {
+    return this.countRecipientNotifications.execute({ recipientId });
+  }
 
   @Post()
   async create(@Body() createNotificationDto: CreateNotificationDto) {
@@ -18,5 +55,23 @@ export class NotificationController {
     });
 
     return { notification: NotificationMapper.toHttp(notification) };
+  }
+
+  @Patch(':id/read')
+  @HttpCode(204)
+  async read(@Param('id') id: string) {
+    await this.readNotification.execute({ notificationId: id });
+  }
+
+  @Patch(':id/unread')
+  @HttpCode(204)
+  async unread(@Param('id') id: string) {
+    await this.unreadNotification.execute({ notificationId: id });
+  }
+
+  @Patch(':id/cancel')
+  @HttpCode(204)
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({ notificationId: id });
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendNotificationUseCase } from '@app/use-cases/send-notification-use-case';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationController } from './controllers/notification.controller';
+import { CancelNotificationUseCase } from '@app/use-cases/cancel-notification-use-case';
+import { ReadNotificationUseCase } from '@app/use-cases/read-notification-use-case';
+import { UnreadNotificationUseCase } from '@app/use-cases/unread-notification-use-case';
+import { GetRecipientNotificationsUseCase } from '@app/use-cases/get-recipient-notifications-use-case';
+import { CountRecipientNotificationsUseCase } from '@app/use-cases/count-recipient-notifications-use-case';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationController],
-  providers: [SendNotificationUseCase],
+  providers: [
+    SendNotificationUseCase,
+    CancelNotificationUseCase,
+    ReadNotificationUseCase,
+    UnreadNotificationUseCase,
+    GetRecipientNotificationsUseCase,
+    CountRecipientNotificationsUseCase,
+  ],
 })
 export class HttpModule {}
